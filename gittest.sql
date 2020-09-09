d5959c46b1ea1f9319486b59ab9a1ce496d14fb7
willy <shl0905@uw.edu>
2020-09-09 06:19:58 -0700

    second try add .java

--- gittest.sql
+++ gittest.sql
@@ -1,1 +1,2 @@
 first attemp
+second attemp
---
+++ j.java
@@ -0,0 +1,9 @@
+Import java.util.*;
+
+class play{
+
+
+	public void static final main(String[] args) {
+		System.out.println("hello git, let's play");
+	}
+}
